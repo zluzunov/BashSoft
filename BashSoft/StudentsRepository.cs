a186ef970b59260f57ea68a9e6e429437dfe8bb4
zluzunov <zluzunov@gmail.com>
2017-06-22 19:33:46 +0300

    Problem 2 ready;

---
+++ BashSoft/StudentsRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BashSoft
+{
+    using System.Collections.Generic;
+
+    public static class StudentsRepository
+    {
+        public static bool IsDataInitialized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> _studentsByCourse;
+    }
+}
