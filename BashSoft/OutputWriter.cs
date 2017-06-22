835f01f49eb57a540868b24c27b31e2eab9612b8
zluzunov <zluzunov@gmail.com>
2017-06-22 20:24:21 +0300

    Problem 5 - previous commit was 4 :)

--- BashSoft/OutputWriter.cs
+++ BashSoft/OutputWriter.cs
@@ -27,5 +27,10 @@
             Console.WriteLine(message);
             Console.ForegroundColor = currentColor;
         }
+
+        public static void PrintStudent(KeyValuePair<string, List<int>> student)
+        {
+            OutputWriter.WriteMessageOnNewLine(string.Format($"{student.Key} - {string.Join(", ", student.Value)}"));
+        }
     }
 }
